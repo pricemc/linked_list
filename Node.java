539e1887b15554ae27ac4817dcdd7d42d48fa9fd
Matthew Price <doggy1199@yahoo.com>
2013-03-06 12:47:10 -0700

    wrote node

--- Node.java
+++ Node.java
@@ -7,4 +7,37 @@
 
 public class Node {
 
+	private int value;
+	private Node next;
+
+	public Node(int value, Node next) {
+		this.value = value;
+		this.next = next;
+	}
+
+	public Node() {
+		this(0, null);
+	}
+
+	public Node(int value) {
+		this(value, null);
+	}
+
+
+	public int getValue() {
+		return value;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	
 }
