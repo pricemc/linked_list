3bf86297cb422da41a87b8d1ff1159352ed0c441
Matthew Price <doggy1199@yahoo.com>
2013-04-15 20:44:29 -0600

    almost done

--- LinkedList.java
+++ LinkedList.java
@@ -250,6 +250,27 @@ class LinkedList {
 		}
 		return a;
 	}
+	public void removeDuplicates() {
+		Node current = head;
+		Node next = current.getNext();
+		if(current.getNext()!=null) {
+			current = current.getNext();
+			if(current.getNext()!=null) {
+				next = current.getNext();
+			}
+		}
+		while(next.getNext()!=null) {
+			next = current.getNext();
+			if(current.getValue()==next.getValue()) {
+				Node thisN = next.getNext();
+				next.setNext(null);
+				current.setNext(thisN);
+			}else{
+				current = next;
+				next = current.getNext();
+			}
+		}
+	}
 
 }
 
--- Main.java
+++ Main.java
@@ -3,17 +3,15 @@ class Main {
 
 	public static void main(String[] args) {
 		LinkedList a = new LinkedList();
-		LinkedList b = new LinkedList();
 		a.push(2);
 		a.push(4);
 		a.push(8);
 		a.push(1);
 		a.push(3);
-		b.buildOneTwoThree();
 		a.print();
-		LinkedList[] c = a.frontBackSplit();
-		c[0].print();
-		c[1].print();
+		a.insertSort();
+		a.print();
+		a.removeDuplicates();
 
 	}
 }
